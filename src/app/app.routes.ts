87f20647dd1b36945a309a60daacae678454d5d4
Mikydosh <michal.dolik@email.cz>
2025-11-22 16:36:16 +0100

    Přidána navigace se 4 taby (filmy, seriály, seznamy, nastavení)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,4 +5,20 @@ export const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
   },
+  {
+    path: 'movies',
+    loadComponent: () => import('./pages/movies/movies.page').then( m => m.MoviesPage)
+  },
+  {
+    path: 'series',
+    loadComponent: () => import('./pages/series/series.page').then( m => m.SeriesPage)
+  },
+  {
+    path: 'lists',
+    loadComponent: () => import('./pages/lists/lists.page').then( m => m.ListsPage)
+  },
+  {
+    path: 'settings',
+    loadComponent: () => import('./pages/settings/settings.page').then( m => m.SettingsPage)
+  },
 ];
--- src/app/tabs/tabs.routes.ts
+++ src/app/tabs/tabs.routes.ts
@@ -7,30 +7,35 @@ export const routes: Routes = [
     component: TabsPage,
     children: [
       {
-        path: 'tab1',
+        path: 'movies',
         loadComponent: () =>
-          import('../tab1/tab1.page').then((m) => m.Tab1Page),
+          import('../pages/movies/movies.page').then((m) => m.MoviesPage),
       },
       {
-        path: 'tab2',
+        path: 'series',
         loadComponent: () =>
-          import('../tab2/tab2.page').then((m) => m.Tab2Page),
+          import('../pages/series/series.page').then((m) => m.SeriesPage),
       },
       {
-        path: 'tab3',
+        path: 'lists',
         loadComponent: () =>
-          import('../tab3/tab3.page').then((m) => m.Tab3Page),
+          import('../pages/lists/lists.page').then((m) => m.ListsPage),
+      },
+      {
+        path: 'settings',
+        loadComponent: () =>
+          import('../pages/settings/settings.page').then((m) => m.SettingsPage),
       },
       {
         path: '',
-        redirectTo: '/tabs/tab1',
+        redirectTo: '/tabs/movies',
         pathMatch: 'full',
       },
     ],
   },
   {
     path: '',
-    redirectTo: '/tabs/tab1',
+    redirectTo: '/tabs/movies',
     pathMatch: 'full',
   },
 ];
